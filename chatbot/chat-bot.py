2d3b18b489a53523e12105791c2889300ccdee32
necteur <79530695+necteur@users.noreply.github.com>
2021-02-23 20:07:28 +0100

    Update chat-bot.py
    plus de description

--- chatbot/chat-bot.py
+++ chatbot/chat-bot.py
@@ -10,7 +10,7 @@ import tensorflow
 import random
 import json
 from tensorflow.python.framework import ops
-with open("train.json") as file:
+with open("train.json") as file: #ouverture du fichier de donné (dictionnaire python)
     data = json.load(file)
 
 words = []
@@ -18,17 +18,17 @@ labels = []
 docs_x = []
 docs_y = []
 
-for intent in data["intents"]: #on parcour tout le data
-    for pattern in intent["patterns"]:
-        wrds = nltk.word_tokenize(pattern) # séparation des mot dans une phrases pour permettre la modularité
-        words.extend(wrds) #ajout de la liste wrds à la liste words
-        docs_x.append(wrds)
+for intent in data["intents"]:      #on parcour tout le data (ici des dictionnaires de donné) qui est comprie dans le dictionnaire intents
+    for pattern in intent["patterns"]:      # on parcour le dictionnaire pour voir les différentes "feature(input que l'IA va devoir faire face)" appeller ici pattern
+        wrds = nltk.word_tokenize(pattern)      # séparation des mot dans une phrases pour permettre la modularité
+        words.extend(wrds)      #ajout de la liste wrds à la liste words pour connaitre les mots qui sont dans la "feature"
+        docs_x.append(wrds)     
         docs_y.append(intent["tag"])
 
         if intent["tag"] not in labels: # ajout de tout les types de donné pour qu'il soit par la suite traité
             labels.append(intent["tag"])
 
-words = [stemmer.stem(w.lower()) for w in words if w not in "?"]
+words = [stemmer.stem(w.lower()) for w in words if w not in "?"] #permet d'avoir les mots clefs
 words = sorted(list(set(words)))
 
 labels = sorted(labels)
@@ -58,6 +58,7 @@ for x,doc in enumerate(docs_x):
 training = numpy.array(training)
 output= numpy.array(output)
 
+#création du résaux de neurone
 ops.reset_default_graph()
 net = tflearn.input_data(shape=[None, len(training[0])])
 net = tflearn.fully_connected(net, 8)
