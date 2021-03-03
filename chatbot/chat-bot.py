5371ac290f6a4d4b91939e9d1602bac8bba05e2b
necteur <79530695+necteur@users.noreply.github.com>
2021-03-03 10:25:39 +0100

    mise a jour

--- chatbot/chat-bot.py
+++ chatbot/chat-bot.py
@@ -75,7 +75,7 @@ net = tflearn.regression(net) # prédiction de la sortie à partir de l'entrée
 model= tflearn.DNN(net)
 
 #entrainement de l'IA : n_epch=x le nombre de fois que l'on va entrainer le bot
-model.fit(training, output, n_epoch=10, batch_size=32, show_metric=True)
+model.fit(training, output, n_epoch=1000, batch_size=32, show_metric=True)
 model.save("model.tflearn")
 
 #prédiction
