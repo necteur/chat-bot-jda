1e802a8192b9ae3b9a2f2f92bc8262b1836a0619
necteur <79530695+necteur@users.noreply.github.com>
2021-03-15 14:28:18 +0100

    modif global
    ajout d'un setup + modification du code du chatbot

--- chatbot/chat-bot.py
+++ chatbot/chat-bot.py
@@ -10,84 +10,19 @@ import tensorflow
 import random
 import json
 import time
-import pyglet
 import os
 from tensorflow.python.framework import ops
 with open("train.json", encoding="utf-8") as file: #ouverture du fichier de données (dictionnaire python)
     data = json.load(file)
-
-words = []
-labels = []
-docs_x = []
-docs_y = []
-
-for intent in data["intents"]:      #on parcourt tout le data (ici des dictionnaires de données) qui est compris dans le dictionnaire intents
-    for pattern in intent["patterns"]:      # on parcourt le dictionnaire pour voir les différentes "feature(input que l'IA va devoir faire face)" appellé ici pattern
-        wrds = nltk.word_tokenize(pattern)      # séparation des mots dans une phrase pour permettre la modularité
-        words.extend(wrds)      #ajout de la liste wrds à la liste words pour connaître les mots qui sont dans la "feature"
-        docs_x.append(wrds)
-        docs_y.append(intent["tag"])
-
-        if intent["tag"] not in labels: # ajout de tous les types de données pour qu'ils soient par la suite traités et que aucun ne soit oublié
-            labels.append(intent["tag"])
-
-words = [stemmer.stem(w.lower()) for w in words if w not in "?"] #permet d'avoir la racine des mots et de comprendre le sens des mots ex :bnjr veux dire bonjour, gentiment ==> gentil ce qui va lui permettre de comprendre des mots dérivés (ex : "il est d'un gentille" l'IA va comprendre "il est gentil")
-words = sorted(list(set(words))) # création d'une liste de mots simplifiés qui vont simplifier l'analyse des données
-
-labels = sorted(labels)
-
-training =[]
-output = []
-
-out_empty = [0 for _ in range(len(labels))]
-
-for x,doc in enumerate(docs_x):
-    bag = []
-
-    wrds= [stemmer.stem(w) for w in doc]
-
-    for w in words:
-        if w in wrds:
-            bag.append(1)
-        else:
-            bag.append(0)
-    output_row = out_empty[:]
-    output_row[labels.index(docs_y[x])] = 1
-
-    training.append(bag)
-    output.append(output_row)
-
-
-training = numpy.array(training)
-output= numpy.array(output)
-
-#création du réseau de neurones ici 4 neurones
-ops.reset_default_graph()
-net = tflearn.input_data(shape=[None, len(training[0])]) # couche d'entrer des neurones input
-# couche cachée des neurones qui vont "réfléchir" pour déterminer les règles puis les utiliser (4 couche qui continnent 16 neurones)
-net = tflearn.fully_connected(net, 128)
-net = tflearn.fully_connected(net, 128)
-net = tflearn.fully_connected(net, 128)
-net = tflearn.fully_connected(net, 128)
-net = tflearn.fully_connected(net, len(output[0]), activation="softmax") # sortie des nerones avec "l'activation" qui va permettre de changé des nombres incompréhensibles en probabilité
-#ici la fonction d'activation utilisée est softmax +info : https://fr.wikipedia.org/wiki/Fonction_softmax
-net = tflearn.regression(net) # prédiction de la sortie à partir de l'entrée
-
-model= tflearn.DNN(net)
-
-#entrainement de l'IA : n_epch=x le nombre de fois que l'on va entrainer le bot
-model.fit(training, output, n_epoch=30000, batch_size=128, show_metric=True)
-model.save("model.tflearn")
-
 #prédiction
-def bag_of_words(s, words):
-    bag = [0 for _ in range(len(words))]
+def traitement_des_donnees(s, features):
+    bag = [0 for _ in range(len(features))]
 
-    s_words = nltk.word_tokenize(s) # on découpe les mot d'entrée
-    s_words = [stemmer.stem(word.lower()) for word in s_words] # on prend les mots clefs
+    words_tok = nltk.word_tokenize(s) # on découpe les mot d'entrée
+    words_tok = [stemmer.stem(word.lower()) for word in words_tok] # on prend les mots clefs
 
-    for se in s_words:
-        for i, w in enumerate(words):
+    for se in words_tok:
+        for i, w in enumerate(features):
             if w == se:
                 bag[i] = 1
 
@@ -96,17 +31,17 @@ def bag_of_words(s, words):
 def post(a) :
         #print("Vous pouvez commencer à parler (taper quit pour arrêter)!")
         inp = input_user.get()
-        messages.insert(INSERT, '%s\n' % "vous: ")
         messages.insert(INSERT, '%s\n' % inp)
         if inp.lower() == "quit":
             window.destroy()
+
         if inp.lower() == "bad apple":
             os.system("start cmd /k python ./ASCII_bad_apple-master/run.py")
 
 
-        results = model.predict([bag_of_words(inp, words)])
-        results_index = numpy.argmax(results)
-        tag = labels[results_index]
+        resultat = model.predict([traitement_des_donnees(inp, features)])
+        resultat_index = numpy.argmax(resultat)
+        tag = labels[resultat_index]
 
         for tg in data["intents"]:
             if tg['tag'] == tag:
@@ -116,7 +51,75 @@ def post(a) :
         messages.insert(INSERT, '%s\n' % "Emma:")
         messages.insert(INSERT, '%s\n' % aff_post)
         messages.insert(INSERT, '\n')
+        messages.insert(INSERT, '%s\n' % "vous: ")
         input_field.delete(0, 'end')
+
+
+features = []
+labels = []
+mot1 = []
+mot2 = []
+training =[]
+sortie = []
+
+for intent in data["intents"]:      #on parcourt tout le data (ici des dictionnaires de données) qui est compris dans le dictionnaire intents
+    for pattern in intent["patterns"]:      # on parcourt le dictionnaire pour voir les différentes "feature(input que l'IA va devoir faire face)" appellé ici pattern
+        mots = nltk.word_tokenize(pattern)      # séparation des mots dans une phrase pour permettre la modularité
+        features.extend(mots)      #ajout de la liste mots à la liste features pour connaître les mots qui sont dans la "feature"
+        mot1.append(mots)
+        mot2.append(intent["tag"])
+
+        if intent["tag"] not in labels: # ajout de tous les types de données pour qu'ils soient par la suite traités et que aucun ne soit oublié
+            labels.append(intent["tag"])
+
+features = [stemmer.stem(w.lower()) for w in features if w not in "?"] #permet d'avoir la racine des mots et de comprendre le sens des mots ex :bnjr veux dire bonjour, gentiment ==> gentil ce qui va lui permettre de comprendre des mots dérivés (ex : "il est d'un gentille" l'IA va comprendre "il est gentil")
+features = sorted(list(set(features))) # création d'une liste de mots simplifiés qui vont simplifier l'analyse des données
+
+labels = sorted(labels)
+
+
+
+out_empty = [0 for _ in range(len(labels))]
+
+for x,doc in enumerate(mot1):
+    bag = []
+
+    mots= [stemmer.stem(w) for w in doc]
+
+    for w in features:
+        if w in mots:
+            bag.append(1)
+        else:
+            bag.append(0)
+    sortie_row = out_empty[:]
+    sortie_row[labels.index(mot2[x])] = 1
+
+    training.append(bag)
+    sortie.append(sortie_row)
+
+
+training = numpy.array(training)
+sortie= numpy.array(sortie)
+
+#création du réseau de neurones ici 4 neurones
+ops.reset_default_graph()
+net = tflearn.input_data(shape=[None, len(training[0])]) # couche d'entrer des neurones input
+# couche cachée des neurones qui vont "réfléchir" pour déterminer les règles puis les utiliser (4 couche qui continnent 16 neurones)
+net = tflearn.fully_connected(net, 128)
+net = tflearn.fully_connected(net, 128)
+net = tflearn.fully_connected(net, 128)
+net = tflearn.fully_connected(net, 128)
+net = tflearn.fully_connected(net, len(sortie[0]), activation="softmax") # sortie des nerones avec "l'activation" qui va permettre de changé des nombres incompréhensibles en probabilité
+#ici la fonction d'activation utilisée est softmax +info : https://fr.wikipedia.org/wiki/Fonction_softmax
+net = tflearn.regression(net) # prédiction de la sortie à partir de l'entrée
+
+model= tflearn.DNN(net)
+
+#entrainement de l'IA : n_epch=x le nombre de fois que l'on va entrainer le bot
+model.fit(training, sortie, n_epoch=1000, batch_size=128, show_metric=True)
+model.save("model.tflearn")
+
+
 
 
 #tkinter
@@ -136,6 +139,7 @@ frame = Frame(window)
 input_field.bind("<Return>", post)
 messages.insert(INSERT, '%s\n' % "Vous pouvez commencer à parler (taper quit pour arrêter)")
 messages.insert(INSERT, '\n')
+messages.insert(INSERT, '%s\n' % "vous: ")
 frame.pack()
 
 window.mainloop()
---
+++ setup.py
@@ -0,0 +1,9 @@
+import os
+os.system("pip install nltk")
+os.system("pip install os")
+os.system("pip install numpy")
+os.system("pip install tflearn")
+os.system("pip install tensorflow")
+os.system("pip install tensorflow-gpu")
+os.system("pip install json")
+
